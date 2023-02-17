2dc1777bc93ccd5811b8a3377bb7162090c03030
Kim, Haejin <hkim949@gatech.edu>
2023-02-17 02:16:04 -0500

    3

--- A3_SQLQuery.sql
+++ A3_SQLQuery.sql
@@ -41,8 +41,13 @@ FROM (
 GROUP BY City
 
 --5.union
-
-
+SELECT City, COUNT(CustomerID) NumCustomer
+FROM Customers c
+GROUP BY City
+UNION
+SELECT City, COUNT(CustomerID) NumCustomer
+FROM Customers c
+GROUP BY City
 
 --5.subquery_no_union
 SELECT dt.City
@@ -54,15 +59,45 @@ GROUP BY City
 WHERE dt.NumCustomer >= 2
 
 --6.
+SELECT dt.City
+FROM (SELECT City, COUNT(DISTINCT ProductID) DistProd 
+FROM (
+SELECT OrderID, ProductID
+FROM [Order Details]
+) od JOIN (
+SELECT OrderID, CustomerID
+FROM Orders
+) o ON od.OrderID = o.OrderID JOIN (
+SELECT CustomerID, City
+FROM Customers
+) c ON o.CustomerID = c.CustomerID
+--WHERE City = 'Madrid'
+GROUP BY City
+) dt
+WHERE dt.DistProd >= 2
 
 --7.
+SELECT *
+FROM Customers
+WHERE CustomerID IN (
+SELECT DISTINCT c.CustomerID
+FROM (
+SELECT CustomerID, City
+FROM Customers) c JOIN
+(SELECT OrderID, CustomerID, ShipCity
+FROM Orders) o ON c.CustomerID = o.CustomerID
+WHERE City != ShipCity
+)
 
 --8.
 
+
 --9.subquery
+
 
 --9.no_subquery
 
 --10.join_subquery
 
 --11.
+-- Using GROUP BY, DISTINCT
